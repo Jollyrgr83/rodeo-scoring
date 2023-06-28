8232b3b635c3a88b62c59732b7e1b7de25d942c5
Jason Mangin <jmangin@goldrushexpeditions.com>
2023-06-28 10:49:03 -0600

    added comments for upcoming functionality and removed unused import

--- controllers/report.js
+++ controllers/report.js
@@ -1,6 +1,7 @@
-const db = require("../models");
+// const db = require("../models");
 
 module.exports = {
+    /* controller for upcoming pdf report download functionality */
     retrieveReport: (req, res) => {
         res.setHeader("Content-Type: application/pdf");
         res.setHeader("Content-Disposition: attachment; filename=report.pdf");
